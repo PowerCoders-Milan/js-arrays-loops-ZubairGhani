ad62f5f92f8638db2e785d3bd9cc9cc08d8b8b50
ZubairGhani <ghani.zubairmohammad@gmail.com>
2021-06-18 17:23:44 +0200

    first draft

--- 03-multiples-of-number/multiples_of_number.js
+++ 03-multiples-of-number/multiples_of_number.js
@@ -1,1 +1,31 @@
 // define your function here
+const multiplesOf = (number, limit) => {
+    for (let i = 0; i <= number; i++);
+    if (i % 2 === 0)
+    multiples.push(i);
+    
+}
+console.log(multiplesOf(10, 100));
+
+
+
+
+
+
+
+
+
+/*
+function multiplesOf(numbers, number) { // add second argument
+    var multiples = []; // change to array (so that we can store multiple numbers - not just one multiple)
+    for (var i = 0; i < numbers.length; i++) {
+      if (numbers[i] % number === 0) { // divide by the number
+        multiples.push(numbers[i]); // add the current multiple found to the multiples array
+      }
+    }
+  
+    return multiples;
+  }
+  
+  console.log(multiplesOf([4, 5, 6, 7, 8], 2)); // Output: [4, 6, 8]
+  */
