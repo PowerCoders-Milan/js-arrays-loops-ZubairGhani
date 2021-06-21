d949582c3f16fabfca6096907dc44440b726ba6a
ZubairGhani <ghani.zubairmohammad@gmail.com>
2021-06-21 10:32:19 +0200

    fibonacci ex done

--- 05-fibonacci/fibonacci.js
+++ 05-fibonacci/fibonacci.js
@@ -2,3 +2,14 @@
 const maxNumber = Math.ceil(Math.random()*200);
 let n1 = 0, n2 = 1, nextTerm;
 
+function getFibonacciSequence(maxNumber){
+    let fibArray = []
+    fibArray [0] = 0;
+    fibArray [1] = 1;
+    for (let i = 2; i < maxNumber; i++){
+        fibArray [i] = fibArray[i-1] + fibArray [i-2];
+    }
+    return fibArray;
+}
+ console.log(getFibonacciSequence(maxNumber));
+
